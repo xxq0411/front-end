5b6bdf7ced10b404998432002eee6041c899af9a
xxq0411 <416876921@qq.com>
2018-02-02 15:39:27 +0800

    number guessing game

---
+++ MDNdemo/demo1.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    </head>
+    <body>
+    <button>
+    Click me
+</button>
+</body>
+<script>
+<!--JavaScript goes here-->
+function CreatParagraph (){
+        var para = document.creatElement("p");
+}
+</script>
+</html>
